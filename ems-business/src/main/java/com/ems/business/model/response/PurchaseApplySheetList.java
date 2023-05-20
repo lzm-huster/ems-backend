98012c513b65a3ed963a19191d9b127cc178f5f2
hustjd <1467136004@qq.com>
2023-05-20 23:17:35 +0800

    后端的实体都不要写get，set方法，全都有@Data注解代替，否则json转换会出错

--- ems-business/src/main/java/com/ems/business/model/response/PurchaseApplySheetList.java
+++ ems-business/src/main/java/com/ems/business/model/response/PurchaseApplySheetList.java
@@ -1,13 +1,15 @@
 package com.ems.business.model.response;
 
 
+import lombok.Data;
+
 import java.util.Date;
 
 /**
  * 设备采购申请单列表 Response  来源于PurchaseApplySheet表和User表
  *申请单编号、设备名称（多设备组合成一条字符串）、申请人、申请时间、采购申请状态、责任导师（学生)
  */
-
+@Data
 public class PurchaseApplySheetList {
     /**
      * 采购申请单编号
@@ -38,61 +40,5 @@ public class PurchaseApplySheetList {
      * 责任导师名称
      */
     private String approveTutorName;
-
-    public void SetPurchaseApplySheetID(int PurchaseApplySheetID)
-    {
-        purchaseApplySheetID= PurchaseApplySheetID;
-    }
-
-    public Integer getPurchaseApplySheetID()
-    {
-        return purchaseApplySheetID;
-    }
-
-    public void SetDeviceList(String DeviceList)
-    {
-        deviceList= DeviceList;
-    }
-
-    public String getDeviceList()
-    {
-        return deviceList;
-    }
-    public void SetUserName(String UserName)
-    {
-        userName= UserName;
-    }
-
-    public String getUserName()
-    {
-        return userName;
-    }
-    public void SetPurchaseApplyDate(Date PurchaseApplyDate)
-    {
-        purchaseApplyDate= PurchaseApplyDate;
-    }
-
-    public Date  getPurchaseApplyDate()
-    {
-        return purchaseApplyDate;
-    }
-    public void SetPurchaseApplyState(String PurchaseApplyState)
-    {
-        purchaseApplyState= PurchaseApplyState;
-    }
-
-    public String getPurchaseApplyState()
-    {
-        return purchaseApplyState;
-    }
-    public void SetApproveTutorName(String ApproveTutorName)
-    {
-        approveTutorName= ApproveTutorName;
-    }
-
-    public String getApproveTutorName()
-    {
-        return approveTutorName;
-    }
 
 }
