943294cc8fcee5b33ec41b12170b920c11e4502d
lzm-huster <815925407@qq.com>
2023-06-17 11:28:07 +0800

    0617 处理参数bug

--- ems-business/src/main/java/com/ems/business/model/request/DeviceScrapListUpdateReq.java
+++ ems-business/src/main/java/com/ems/business/model/request/DeviceScrapListUpdateReq.java
@@ -31,7 +31,7 @@ public class DeviceScrapListUpdateReq {
     /**
      * 报废时间
      */
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date scrapTime;
 
     /**
